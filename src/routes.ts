0f481d287afe99d292d7b36be610f33bd2b52bcb
Anthony Gubler <agubler@sitepen.com>
2020-06-26 12:17:39 +0100

    example failure

---
+++ src/routes.ts
@@ -0,0 +1,18 @@
+export default [
+	{
+		id: 'home',
+		path: 'home/home/home',
+		outlet: 'main',
+		defaultRoute: true
+	},
+	{
+		id: 'about',
+		path: 'about/about/about',
+		outlet: 'main'
+	},
+	{
+		id: 'profile',
+		path: 'profile/profile/profile',
+		outlet: 'main'
+	}
+];
